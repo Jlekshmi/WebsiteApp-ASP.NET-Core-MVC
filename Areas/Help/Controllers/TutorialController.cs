1b87e3495c65b82bbc371ed80c50e6e5062ffadc
Jayalekshmi Vrinda Kumari Jayachandran <jayalekshmivj08@gmail.com>
2026-02-26 02:30:49 -0500

    Add project files.

---
+++ Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsiteApp.Areas.Help.Controllers
+{
+    [Area("Help")]
+    public class TutorialController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            if (id == "Page1")
+            {
+                return View("Page1");
+            }
+            else if (id == "Page2")
+            {
+                return View("Page2");
+            }
+            else if (id == "Page3")
+            {
+                return View("Page3");
+            }
+            return View("Page1"); // Default to Page1 if no id is provided
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsiteApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            // Contact information to be passed to the Contact view
+            var contactInfo = new List<string>
+            {
+                "Phone: 555-123-4567",
+                "Email: me@mywebsite.com",
+                "Facebook: facebook.com/mywebsite"
+            };
+
+            ViewBag.ContactInfo = contactInfo;
+
+            return View();
+        }
+    }
+}
